e95d4aab201339bb37ca69a0edd492c68b47cdff
Lane G Smith <lane.smith.contact@gmail.com>
2023-09-18 12:32:00 -0700

    Configure Dependencies configure tailwind.config create watch script update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import '../public/output.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="App" class="">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
