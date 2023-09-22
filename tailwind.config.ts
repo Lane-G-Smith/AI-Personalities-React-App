0eea8e20939e81a22874b17b5bd1202b9d5b2ea9
Lane G Smith <lane.smith.contact@gmail.com>
2023-09-22 13:37:53 -0700

    Fix tailwind.confid add title

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import { type Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,jsx}'],
+	content: ['./src/**/*.{html,js,jsx,tsx}'],
 	theme: {
 		extend: {},
 		colors: {
