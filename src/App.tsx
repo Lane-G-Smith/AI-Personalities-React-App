c1d0ab0bdd1370e1a00f131a7934316b004b9184
Lane G Smith <108886772+Lane-G-Smith@users.noreply.github.com>
2023-09-24 11:41:42 -0700

    Added broken dark mode toggle (#11)
    * Added broken dark mode toggle
    work in progress
    need to fix state change issue
    
    * Update package
    
    * Fix runtime errors, module names
    
    * Merge changes
    
    * Run Tailwind, add checks, update TypeScript config
    
    ---------
    
    Co-authored-by: akpi816218 <816218@seq.org>

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import '../public/output.css';
+import DarkBtn from './components/DarkMode/DarkBtn'
+import './output.css'
 
-function App() {
-	const [count, setCount] = useState(0);
-
+export default function App() {
 	return (
-		// ? is this a class or a className??
-		<div className="App" /*class=""*/>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+		<div className="App">
+			<DarkBtn />
 		</div>
-	);
+	)
 }
-
-export default App;
