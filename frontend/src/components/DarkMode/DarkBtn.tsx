be073be92942407decb3cd993af88f2f990832f3
akpi816218 <816218@seq.org>
2023-09-27 13:16:50 -0700

    Patch changes by @Sbrown19

--- frontend/src/components/DarkMode/DarkBtn.tsx
+++ frontend/src/components/DarkMode/DarkBtn.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 
 export default function DarkBtn() {
+	const [, setThemeInit] = useState(true);
+
+	const userTheme = localStorage.getItem('theme'),
+		systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	if (userTheme === 'dark' || systemTheme) setThemeInit(true);
+	else setThemeInit(false);
+
+	const [darkTheme, setTheme] = useState(userTheme === 'dark' || systemTheme);
+
 	return (
-		<button
+		<input
 			id="theme-toggle"
-			type="button"
-			className="p-4 my-2 flex text-blue-700 dark:bg-red-500 hover:scale-110 duration-100 hover:duration-100 rounded-lg text-sm ">
+			checked={darkTheme}
+			type="checkbox"
+			onClick={() => toggleTheme(darkTheme, setTheme)}
+			aria-label="Dark mode toggle"
+			className="p-4 my-2 flex text-blue-700 dark:bg-red-500 hover:scale-110 duration-100 hover:duration-100 rounded-lg text-sm "
+		>
 			<span className="p-1 font-bold fa-large fa-moon">Dark</span>
-		</button>
-	)
+		</input>
+	);
 }
+
+function toggleTheme(isDark: boolean, setDarKMode: React.Dispatch<React.SetStateAction<boolean>>) {
+	isDark = !isDark;
+	isDark ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark');
+	localStorage.setItem('theme', isDark ? 'dark' : 'light')
+	setDarKMode(isDark);
+};
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,22 +1,5 @@
 import { type Config } from 'tailwindcss';
-// This is to import react so we can use it for dark mode.
-import React, {useState} from 'react';
-import ReactDom from 'react-dom/client'
 
-{/* This is my attempt at getting darkmode to work in React.
-export const DarkModeToggle = () => {
-	const [isDark, setIsDark] = useState(true);
-  
-	return (
-	  <Toggle
-		checked={isDark}
-		onChange={({ target }) => setIsDark(target.checked)}
-		icons={{ checked: "🌙", unchecked: "🔆" }}
-		aria-label="Dark mode toggle"
-	  />
-	);
-  };
-*/}
 export default {
 	content: ['./src/**/*.{html,js,jsx,tsx}'],
 	theme: {
