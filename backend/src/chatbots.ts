bd128573b615b4c523f840f4310f3a2a9a0971c1
Akhil Pillai <gh@akpi.is-a.dev>
2023-10-01 17:50:12 -0700

    Fix escaping strings

--- backend/src/chatbots.ts
+++ backend/src/chatbots.ts
@@ -30,23 +30,25 @@ class ChatBotManager {
 	async getChatCompletion(botName: string, username: string, prompt: string) {
 		const chatBot = this.bots.get(botName);
 		if (!chatBot) return null;
-		return (
-			await this.client.chat.completions.create({
-				model: 'gpt-3.5-turbo-0613',
-				messages: [
-					{
-						role: 'system',
-						content: chatBot.prompt
-					},
-					{
-						role: 'user',
-						name: username,
-						content: prompt
-					}
-				],
-				max_tokens: 100
-			})
-		).choices[0].message;
+		return decodeURIComponent(
+			(
+				await this.client.chat.completions.create({
+					model: 'gpt-3.5-turbo-0613',
+					messages: [
+						{
+							role: 'system',
+							content: chatBot.prompt
+						},
+						{
+							role: 'user',
+							name: username,
+							content: prompt
+						}
+					],
+					max_tokens: 100
+				})
+			).choices[0].message.content ?? 'No response'
+		);
 	}
 }
 
