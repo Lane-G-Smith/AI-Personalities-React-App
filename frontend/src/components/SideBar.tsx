af640ebf603c36007dc781127eb0fb73eb97b01b
akpi816218 <816218@seq.org>
2023-10-02 20:15:52 -0700

    chore: lint

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,4 +1,4 @@
-import DarkBtn from "./DarkBtn";
+import DarkBtn from './DarkBtn';
 
 export default function SideBar() {
 	return <section id="sidebar" className="flex flex-col w-1/4 min-h-full h-full m-0 p-0 border-solid border-2 border-emerald-500 hover:w-1/2" style={{
