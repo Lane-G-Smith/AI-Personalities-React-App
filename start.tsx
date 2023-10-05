b282d823c86afc80b64e049b9a24474ac2b0e38f
akpi816218 <816218@seq.org>
2023-10-05 09:30:10 -0700

    Update start scripts, linting, packages

--- start.tsx
+++ start.tsx
@@ -1,41 +1,21 @@
-import { createServer } from 'vite';
-import { join } from 'path';
-import postcss from 'postcss';
-import tailwindcss from 'tailwindcss';
-import myConfig from './frontend/tailwind.config';
-import { readFile, writeFile } from 'fs/promises';
-import { chdir, cwd } from 'process';
-import nodemon from 'nodemon';
+import { spawn } from 'child_process';
+import { stdout } from 'process';
 
-chdir('frontend');
+const processes = [
+	spawn('npm urn dev', { shell: true, cwd: 'backend' }),
+	spawn('npm urn watch', { shell: true, cwd: 'frontend' }),
+	spawn('npm urn dev', { shell: true, cwd: 'frontend' })
+];
+for (const p of processes) {
+	p.stdout.on('data', data => {
+		stdout.write(data.toString());
+	});
+	p.stderr.on('data', data => {
+		stdout.write(data.toString());
+	});
+}
 
-const dirname = cwd();
-
-const server = await createServer({
-	root: dirname,
-	configFile: join(dirname, 'vite.config.ts')
+process.on('exit', () => {
+	for (const p of processes) p.kill();
+	stdout.write('\nKilled all processes');
 });
-await server.listen();
-
-let bufferOld = '', bufferNew = '';
-(async () => {
-	const inputFilePath = './src/input.css', outputFilePath = './src/output.css';
-	bufferNew = await readFile(inputFilePath, 'utf8');
-	if (bufferOld === bufferNew) return;
-	await writeFile(outputFilePath, (await postcss([tailwindcss(myConfig)]).process((bufferNew), {
-		from: inputFilePath,
-		to: outputFilePath
-	})).css);
-	bufferOld = bufferNew;
-})();
-
-const daemon = nodemon({
-	cwd: '../backend',
-	script: 'src/server.ts',
-	execMap: {
-		ts: 'tsx'
-	},
-	ext: 'ts,json',
-	watch: ['.']
-});
-daemon.on('crash', () => daemon.restart());
