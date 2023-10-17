932f74e0c296f4f538aa31f9a5aeab43d67f260e
Akhil Pillai <gh@akpi.is-a.dev>
2023-10-16 21:14:27 -0700

    (broken) add ClickVote

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -3,6 +3,7 @@ import { chatBots } from '../../../common/chatBots';
 import BotSidebarTab from './BotSidebarTab';
 import DarkBtn from './DarkBtn';
 import { ReactStateFunction } from '../types';
+import ClickVoteLikes from './ClickVoteLikes';
 
 export default function SideBar({
 	currentBot,
@@ -17,7 +18,7 @@ export default function SideBar({
 			className="flex flex-grow-0 flex-col w-1/5 min-h-screen h-screen m-0 p-2 border-solid border-2 border-emerald-500"
 		>
 			<div id="topbar" className="flex-auto flex-grow-0">
-				{/* Input element for name, may be useful in future */}
+				{/* Input element for name */}
 				<input
 					type="text"
 					name="name"
@@ -26,6 +27,7 @@ export default function SideBar({
 					placeholder="Name"
 					autoComplete="name"
 				/>
+				<ClickVoteLikes />
 				<DarkBtn />
 			</div>
 
